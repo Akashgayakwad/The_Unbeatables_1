2be3371408c37ac2ba47954ceb443f2f8196864f
kaushik27mishra <kmishra27.km@gmail.com>
2020-01-13 19:43:22 +0530

    form cards added

--- src/Containers/Main/Main.js
+++ src/Containers/Main/Main.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import {BrowserRouter,Route,Link} from 'react-router-dom';
+import Mainform from '../../Components/AerodromeLicensing/Mainform'
 class Main extends Component{
     render(){
         return(
             <BrowserRouter>
                 <div className="container">
-                    
+                    <Route exact path to='./license'>
+                        <Mainform/>
+                    </Route>
                 </div>
             </BrowserRouter>
         )
