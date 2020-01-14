4e72639f3f219f5ded9279ad3e3100ed3834ab6c
Akashgayakwad <akashgayakwad123@gmail.com>
2020-01-14 10:04:06 +0530

    searchbar added

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React,{Component} from 'react';
 import Displaycardlist from './../Card/Displaycard/Displaycardlist';
+import Searchbar from './../Card/Searchbar/Searchbar';
 class Dashboard extends Component{
     render(){
         return(
             <div>
+                <Searchbar/>
                 <Displaycardlist/>
             </div>
         )
