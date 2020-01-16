a215c3d0ff5c38f940fb3ac0578891ad3b54d3db
kaushik27mishra <kmishra27.km@gmail.com>
2020-01-17 02:59:50 +0530

    Bug Fix in Table

--- src/Containers/Footer/Footer.js
+++ src/Containers/Footer/Footer.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Footer extends Component{
     render(){
         return(
-            <div class="container-fluid footer fixed-bottom mt-10" style={{backgroundColor:'#23374d',height:'50px'}}>
+            <nav class="container-fluid footer fixed-bottom mt-10" style={{backgroundColor:'#23374d',height:'50px'}}>
                 <p class="text-center" style={{color:'white'}}>All right reserved &copy; The Unbeatables</p>
-            </div>
+            </nav>
         )
     }
 }
